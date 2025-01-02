3f96752a4cc3d340dfdb2b8a2b4541e98b059fbc
Zackdokong <66398105+Zackdokong@users.noreply.github.com>
2025-01-03 01:41:37 +0900

    로그인 만드는중 잘거임

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainPage from './mainpage';
 import SignUp from './components/signup'
+import LogIn from './components/login'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />}></Route>
         <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/login" element={<LogIn />}></Route>
       </Routes>
     </Router>
   );
---
+++ src/components/login.tsx
@@ -0,0 +1,14 @@
+import "./signup.css";
+import Header from "./header";
+import { useState } from "react";
+import supabase from "../../supabase";
+
+function LogIn() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default LogIn;
